51887d0d9d4a59ca1d5f818ac5039a6204604a81
Dan Vizor <danielvizor@protonmail.com>
2021-07-27 15:52:36 +0100

    first push

--- decrypter.py
+++ decrypter.py
@@ -25,3 +25,75 @@ SOFTWARE.
 """
 
 import os
+
+# Caesar cipher
+def DecryptCaesar(CypherText:str, key:int):
+	out = ""
+	for char in CypherText.lower():
+		if ord(char) not in range(ord('a'), ord('z')+1):
+			out += char
+			continue
+
+		ClearOrd = ord(char) - key
+		if ClearOrd < ord('a'): ClearOrd += 26
+
+		out += chr(ClearOrd)
+
+	return out
+
+def DecryptSubstitution(CypherText:str, key:dict):
+	out = ""
+
+	return out
+
+# import the common words list from file
+CommonWords = [i.rstrip() for i in open("1-100k.txt", "r")]
+
+# get input from user
+CypherText = input("enter cyphertext: ")
+
+# try every avalable key for a likely decrypt
+results = []
+for key in range(1, 26):
+	ClearText = DecryptCaesar(CypherText, key)
+
+	# count the number of words that appear in the common words list
+	FoundWords = 0
+	for word in ClearText.split(" "):
+		for CommonWord in CommonWords:
+			if word == CommonWord: FoundWords += 1
+
+	results.append({"ClearText": ClearText, "key": key, "FoundWords": FoundWords})
+
+# sort results by number of found results
+results = sorted(results, key=lambda k: k['FoundWords'], reverse=True)
+
+# get command line width for output styling
+CMDWidth = os.get_terminal_size().columns
+
+#print results to user
+if [result['FoundWords'] for result in results][0] > 5:
+	for result in results:
+		if result['FoundWords'] > 5:
+			OutMessage = f"{result['ClearText']}"
+			OutKey = f"== key {result['key']} "
+
+			if len(OutMessage) <= CMDWidth:
+				print(f"\n{OutKey}{'='*(len(OutMessage)-len(OutKey))}\n{OutMessage}\n{'='*len(OutMessage)}")
+
+			else:
+				print(f"\n{'='*CMDWidth}\n{OutMessage}\n{'='*CMDWidth}")
+
+# if no clear decrypts are found (or a very short message was entered) 
+else:
+	if len(CypherText.split(" ")) < 5: print("No likely decrypts found, showing low quality results.")
+
+	for result in sorted(results, key=lambda k: k['key']):
+		OutMessage = f"{result['ClearText']}"
+		OutKey = f"== key {result['key']} "
+
+		if len(OutMessage) <= CMDWidth:
+			print(f"\n{OutKey}{'='*(len(OutMessage)-len(OutKey))}\n{OutMessage}\n{'='*len(OutMessage)}")
+
+		else:
+			print(f"\n{'='*CMDWidth}\n{OutMessage}\n{'='*CMDWidth}")
